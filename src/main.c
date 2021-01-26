c2ae0c8dbe9b506fb75639979562d0ec128265d8
Alex Gustafsson <alex-gustafsson@live.se>
2021-01-26 16:54:35 +0100

    Read from STDIN if no file was specified

--- src/main.c
+++ src/main.c
@@ -3,35 +3,36 @@
 
 #include "main.h"
 
-int yyerror(char *c) {
-  printf("It's a bad: %s\n", c);
-  return 0;
-}
-
 int main(int argc, char **argv) {
 #ifdef DEBUG
   printf("Running in debug mode\n");
 #endif
 
-  if (argc == 1)
-    die_with_usage(argv);
-
-  for (int i = 1; i < argc; i++) {
-    FILE *file = fopen(argv[i], "r");
-    if (file == NULL) {
-      perror("Unable to open file");
-      exit(1);
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      FILE *file = fopen(argv[i], "r");
+      lex_file(file);
     }
-
-    yyset_in(file);
-    yylex();
-    fclose(file);
+  } else {
+    lex_file(stdin);
   }
 
   return 0;
 }
 
+void lex_file(FILE *file) {
+  if (file == NULL) {
+    perror("Unable to open file");
+    exit(1);
+  }
+
+  yyset_in(file);
+  yylex();
+  fclose(file);
+}
+
 void die_with_usage(char **argv) {
-  printf("%s: <source file 1> [source file 2, source file 3, ...]\n", argv[0]);
+  printf("%s: [source file 1, source file 2, ...]\n", argv[0]);
+  printf("If no file is specified, STDIN is used");
   exit(1);
 }
--- src/main.h
+++ src/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 extern int yylex();
 extern int yyset_in();
 
 void die_with_usage();
+void lex_file(FILE *file);
 
 #endif
