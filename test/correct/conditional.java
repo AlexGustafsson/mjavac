39f10de3cf30cf7012e65cb8bef0106f33c2725f
Alex Gustafsson <alex-gustafsson@live.se>
2021-02-27 10:42:03 +0100

    Add missing test header

--- test/correct/conditional.java
+++ test/correct/conditional.java
@@ -1,3 +1,7 @@
+// mjavac test file header
+// header: 4
+// parse: succeed - valid syntax
+// semantics: succeed - valid program
 class Conditionals {
   public static void main(int i) {
     if (i == 0) {
