a1e551064e07c370617d1cf588c3806996801b82
Alex Gustafsson <alex-gustafsson@live.se>
2021-03-04 17:00:03 +0100

    Implement reusable methods for graph rendering

--- mjavac/main.cc
+++ mjavac/main.cc
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <map>
 #include <sstream>
-#include <string>
 
 #ifdef GRAPHVIZ_SUPPORT
 #include <graphviz/gvc.h>
@@ -99,51 +98,13 @@ int main(int argc, char **argv) {
     program_node->generate_parse_graph(raw_dot_stream);
     std::string graph_string = raw_dot_stream.str();
 
-    if (ast_dot_path != nullptr) {
-      std::ofstream dot_stream;
-      dot_stream.open(ast_dot_path);
-      if (!dot_stream.is_open()) {
-        std::cerr << "\033[1mmjavac: \033[31merror:\033[0m " << ast_dot_path << ": " << strerror(errno) << std::endl;
-        std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unable to create output dot file" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-      dot_stream << graph_string;
-      dot_stream.close();
-    }
+    if (ast_dot_path != nullptr)
+      write_dot(ast_dot_path, graph_string);
 
 #ifdef GRAPHVIZ_SUPPORT
     char *ast_graph_path = parameter(argc, argv, "--ast-graph");
-    if (ast_graph_path != nullptr) {
-      std::filesystem::path parsed_ast_graph_path(ast_graph_path);
-      std::string extension = parsed_ast_graph_path.extension();
-
-      if (extension.compare(".pdf") != 0 && extension.compare(".png") != 0 && extension.compare(".jpg") != 0) {
-        std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unsupported graph format '" << extension << "'. Expected one of '.pdf', '.png', '.jpg'" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      std::ofstream graph_stream;
-      graph_stream.open(ast_graph_path);
-      if (!graph_stream.is_open()) {
-        std::cerr << "\033[1mmjavac: \033[31merror:\033[0m " << ast_graph_path << ": " << strerror(errno) << std::endl;
-        std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unable to create output graph file" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      GVC_t *context = gvContext();
-      Agraph_t *graph = agmemread(graph_string.c_str());
-      gvLayout(context, graph, "dot");
-      char *graph_data = nullptr;
-      unsigned int graph_data_length = 0;
-      gvRenderData(context, graph, extension.substr(1).c_str(), &graph_data, &graph_data_length);
-      gvFreeLayout(context, graph);
-      agclose(graph);
-      gvFreeContext(context);
-
-      graph_stream.write(graph_data, graph_data_length);
-      graph_stream.close();
-      gvFreeRenderData(graph_data);
-    }
+    if (ast_graph_path != nullptr)
+      render_graph(ast_graph_path, graph_string);
 #endif
   }
 
@@ -188,51 +149,12 @@ int main(int argc, char **argv) {
     cfg->write(raw_dot_stream);
     std::string graph_string = raw_dot_stream.str();
 
-    if (cfg_dot_path != nullptr) {
-      std::ofstream dot_stream;
-      dot_stream.open(cfg_dot_path);
-      if (!dot_stream.is_open()) {
-        std::cerr << "\033[1mmjavac: \033[31merror:\033[0m " << cfg_dot_path << ": " << strerror(errno) << std::endl;
-        std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unable to create output dot file" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-      dot_stream << graph_string;
-      dot_stream.close();
-    }
+    if (cfg_dot_path != nullptr)
+      write_dot(cfg_dot_path, graph_string);
 
 #ifdef GRAPHVIZ_SUPPORT
-    char *cfg_graph_path = parameter(argc, argv, "--cfg-graph");
-    if (cfg_graph_path != nullptr) {
-      std::filesystem::path parsed_cfg_graph_path(cfg_graph_path);
-      std::string extension = parsed_cfg_graph_path.extension();
-
-      if (extension.compare(".pdf") != 0 && extension.compare(".png") != 0 && extension.compare(".jpg") != 0) {
-        std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unsupported graph format '" << extension << "'. Expected one of '.pdf', '.png', '.jpg'" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      std::ofstream graph_stream;
-      graph_stream.open(cfg_graph_path);
-      if (!graph_stream.is_open()) {
-        std::cerr << "\033[1mmjavac: \033[31merror:\033[0m " << cfg_graph_path << ": " << strerror(errno) << std::endl;
-        std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unable to create output graph file" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      GVC_t *context = gvContext();
-      Agraph_t *graph = agmemread(graph_string.c_str());
-      gvLayout(context, graph, "dot");
-      char *graph_data = nullptr;
-      unsigned int graph_data_length = 0;
-      gvRenderData(context, graph, extension.substr(1).c_str(), &graph_data, &graph_data_length);
-      gvFreeLayout(context, graph);
-      agclose(graph);
-      gvFreeContext(context);
-
-      graph_stream.write(graph_data, graph_data_length);
-      graph_stream.close();
-      gvFreeRenderData(graph_data);
-    }
+    if (cfg_graph_path != nullptr)
+      render_graph(cfg_graph_path, graph_string);
 #endif
   }
 
@@ -248,3 +170,50 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+void write_dot(std::string dot_path, std::string graph_string) {
+  std::ofstream dot_stream;
+  dot_stream.open(dot_path);
+  if (!dot_stream.is_open()) {
+    std::cerr << "\033[1mmjavac: \033[31merror:\033[0m " << dot_path << ": " << strerror(errno) << std::endl;
+    std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unable to create output dot file" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  dot_stream << graph_string;
+  dot_stream.close();
+}
+
+#ifdef GRAPHVIZ_SUPPORT
+void render_graph(std::string graph_path, std::string graph_string) {
+  std::filesystem::path parsed_graph_path(graph_path);
+  std::string extension = parsed_graph_path.extension();
+
+  if (extension.compare(".pdf") != 0 && extension.compare(".png") != 0 && extension.compare(".jpg") != 0) {
+    std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unsupported graph format '" << extension << "'. Expected one of '.pdf', '.png', '.jpg'" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  std::ofstream graph_stream;
+  graph_stream.open(graph_path);
+  if (!graph_stream.is_open()) {
+    std::cerr << "\033[1mmjavac: \033[31merror:\033[0m " << graph_path << ": " << strerror(errno) << std::endl;
+    std::cerr << "\033[1mmjavac: \033[31mfatal error:\033[0m unable to create output graph file" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  GVC_t *context = gvContext();
+  Agraph_t *graph = agmemread(graph_string.c_str());
+  gvLayout(context, graph, "dot");
+  char *graph_data = nullptr;
+  unsigned int graph_data_length = 0;
+  gvRenderData(context, graph, extension.substr(1).c_str(), &graph_data, &graph_data_length);
+  gvFreeLayout(context, graph);
+  agclose(graph);
+  gvFreeContext(context);
+
+  graph_stream.write(graph_data, graph_data_length);
+  graph_stream.close();
+  gvFreeRenderData(graph_data);
+}
+
+#endif
--- mjavac/main.hpp
+++ mjavac/main.hpp
@@ -1,4 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string>
+
+void write_dot(std::string dot_path, std::string graph_string);
+void render_graph(std::string graph_path, std::string graph_string);
+
 #endif
